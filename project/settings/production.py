e3b9d37a056d88373e78c74f4bdcc71fdb7f55c9
VirgoDev <kayson@virgodev.com>
2024-11-15 14:06:08 -0700

    removed env check in production.py

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,15 +1,10 @@
 from .base import * # noqa
 import os
-import environ
 
 DIRNAME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(DIRNAME)
 
 DEBUG = False
-
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, ".env"))
-SECRET_KEY = env("SECRET_KEY")
 
 ALLOWED_HOSTS = [
 ]
