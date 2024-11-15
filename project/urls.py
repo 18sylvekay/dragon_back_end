0198d696e41865b77e0e88516d16cccef7845c3b
VirgoDev <kayson@virgodev.com>
2024-11-15 11:07:49 -0700

    added virgodev user basics

--- project/urls.py
+++ project/urls.py
@@ -6,10 +6,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from project.users.api import views as UserViews
 from project.app_launch_api_view import AppLaunchAPIView
+from virgodev_user_basics.api.urls import router as virgodev_user_basics_router
 
 router = DefaultRouter()
 
 router.register("users", UserViews.UserViewSet, basename="users")
+
+router.registry.extend(virgodev_user_basics_router.registry)
 
 urlpatterns = [
     path("api/", include(router.urls)),
